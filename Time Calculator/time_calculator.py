2e38aac77d3cfac4af6c1017e8a80d6716576499
Andrew Ipu <ipuandrew@gmail.com>
2023-01-31 14:46:48 +0300

    Cleaned up comments

--- Time Calculator/time_calculator.py
+++ Time Calculator/time_calculator.py
@@ -2,7 +2,7 @@ import re
 
 week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-#def add_time():
+
 def add_time(start, duration, days=None): #<- need three parameters
 
     #Extract the "Start Hour"
@@ -15,17 +15,14 @@ def add_time(start, duration, days=None): #<- need three parameters
     AM_PM = re.findall('\S[PM]',str (start))[0]
 
     #Extract duration_hours
-    #\s\d+(?=:) <- works so far...
     dur_hours = re.findall('\d\S*(?=:)', str (duration))[0]
 
     #Extract dur_minutes
     dur_minutes = re.findall('(?=\d+)\d+', str (duration))[1]
 
-    #Extract the index of the day input by the user
-    #input_day = start[2]
-    #index = week_days.index(input_day) #index version of weekdays
+    
     try:
-
+        #Extract the index of the day input by the user
         index = next((i for i, day in enumerate(week_days) if day.lower() == days.lower()), None)
 
     except:
@@ -35,9 +32,9 @@ def add_time(start, duration, days=None): #<- need three parameters
     day_count = 0
 
 
-        #add 12 if it is in PM format
+    #add 12 if it is in PM format
     if AM_PM == 'PM':
-            start_hour = int(start_hour) + 12 #I am adding 12(hrs) to convert start_hour to 24hr clock system.
+            start_hour = int(start_hour) + 12 #Adding 12(hrs) to convert start_hour to 24hr clock system.
             new_hour = int(start_hour) + int(dur_hours)
 
             #calculate the number of days + current hours
@@ -48,6 +45,7 @@ def add_time(start, duration, days=None): #<- need three parameters
             
             day_count = day_count + days_hours[0]
 
+            #Error handling in case index == None
             try:
             #Get new day's index on the list
                 new_day_index = (day_count + index) % len(week_days)
@@ -55,15 +53,16 @@ def add_time(start, duration, days=None): #<- need three parameters
             except:
                 new_day_index = None
 
+            #Error handling in case new_day_index == None
             try:
                 new_day = week_days[new_day_index]
             except:
                 new_day = None
 
 
-            #new minutes
+            #new minutes, increment new_hour, increment day_count
             new_min = int(start_min) + int(dur_minutes)
-            if new_min >=60: #Should this be 59 instead? Because at 60, is when we increment an hour/minute/second.
+            if new_min >=60: 
                 new_hour = new_hour + 1
                 new_min = new_min - 60
                 try:
@@ -72,6 +71,7 @@ def add_time(start, duration, days=None): #<- need three parameters
                         new_day_index = new_day_index + 1
                         new_day = week_days[new_day_index]
                 except:
+                    #There's nothing here, so pass...prevent throwing error
                     pass
 
                     
@@ -82,7 +82,6 @@ def add_time(start, duration, days=None): #<- need three parameters
         new_min = int(start_min) + int(dur_minutes)
 
         days_hours = divmod(new_hour, 24)
-            #print(days_hours)
 
         new_hour = days_hours[1] #Assuming this is in 24hrs.
 
@@ -101,11 +100,8 @@ def add_time(start, duration, days=None): #<- need three parameters
             new_day = None
 
         
-
-         
         if new_min >=60:
             new_hour = new_hour + 1
-            #print(new_hour)
             new_min = new_min - 60
             
                 
@@ -132,10 +128,10 @@ def add_time(start, duration, days=None): #<- need three parameters
 
     #check if time is in 24hrs and convert to 12hrs
     if new_hour > 12:
-        new_hour=new_hour-12
+        new_hour = new_hour-12
 
-    elif new_hour <=12:
-        new_hour=new_hour
+    elif new_hour <= 12:
+        new_hour = new_hour
 
     
     new_hour = str(new_hour)
@@ -158,7 +154,6 @@ def add_time(start, duration, days=None): #<- need three parameters
             new_time = new_hour + ":" + new_min + " " + period + ", " + new_day + days_elapsed
 
         else:
-            #print new time in hh : mm format.
             new_time = new_hour + ":" + new_min + " " + period + ", " + new_day
         
     except:
@@ -174,10 +169,3 @@ def add_time(start, duration, days=None): #<- need three parameters
 
 
     return new_time
-
-#Pending:
-    #- Allow user to enter mix of big and small characters for the day. -> DONE
-    #- Allow user to not include day in the calculation. -> DONE
-    #- ADD FEATURE -> display the number of days later. -> use the divmod calculations 
-    # - FIX UNIT TESTS -> DONE
-    #- EFFICIENCY -> Get rid of repeated code sections. -> I'll see if I can replace them with functions.
